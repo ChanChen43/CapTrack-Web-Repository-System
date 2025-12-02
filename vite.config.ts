66c19b72ca36c126cf8d95a665b38d98d7921a86
ChanChen43 <christianlouissuico18@gmail.com>
2025-12-02 21:06:42 +0800

    removed a similar vitejs import

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
-  import react from '@vitejs/plugin-react';
   
+
 
   export default defineConfig({
     plugins: [react()],
